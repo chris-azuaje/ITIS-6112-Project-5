33b1b1737f068d6a8f0c7dce973fc39ae919fe5e
Chris Azuaje <christopherazuaje@gmail.com>
2024-09-30 13:59:15 -0400

    userList update

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {
+  ListItemAvatar,
+  Avatar,
   Divider,
   List,
   ListItem,
   ListItemText,
   Typography,
-}
-from '@mui/material';
+} from '@mui/material';
+
+// import ImageIcon from '@mui/icons-material/Image';
+
 import './userList.css';
+
+const users = window.models.userListModel();
 
 /**
  * Define UserList, a React component of project #5
@@ -20,26 +26,32 @@ class UserList extends React.Component {
   render() {
     return (
       <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
+        <Typography variant='body1'>
+          This is the user list, which takes up 3/12 of the window. You might
+          choose to use <a href='https://mui.com/components/lists/'>Lists</a>{' '}
+          and <a href='https://mui.com/components/dividers/'>Dividers</a> to
           display your users like so:
         </Typography>
-        <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+        <List component='nav'>
+          {users.map((user) => (
+            <>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar
+                    alt='#'
+                    src={window.models.photoOfUserModel(user._id[0].file_name)}
+                  />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={user.first_name + ' ' + user.last_name}
+                  secondary={user.description}
+                />
+              </ListItem>
+              <Divider />
+            </>
+          ))}
         </List>
-        <Typography variant="body1">
+        <Typography variant='body1'>
           The model comes in from window.models.userListModel()
         </Typography>
       </div>
