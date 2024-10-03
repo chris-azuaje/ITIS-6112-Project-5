b28157351ecae3b84994bcca98339be5711ac1f6
Michael-Gain <mgain1@charlotte.edu>
2024-10-03 09:29:12 -0400

    made userList fetch users from webserver

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -14,8 +14,9 @@ import {Link} from 'react-router-dom';
 // import ImageIcon from '@mui/icons-material/Image';
 
 import './userList.css';
+import fetchModel from '../../lib/fetchModelData';
 
-const users = window.models.userListModel();
+//const users = window.models.userListModel();
 
 /**
  * Define UserList, a React component of project #5
@@ -23,40 +24,59 @@ const users = window.models.userListModel();
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+
+	this.state = {
+		users: [],
+	};
+
+	this.getUsers();
+  }
+
+  getUsers () {
+	fetchModel(`/user/list`)
+	.then(
+		(data) => { this.setState( {users: data.data} ); },
+		(err) => { console.log(err); }
+	);
   }
 
   render() {
     return (
-      <div>
-        <Typography variant='body1'>
-          This is the user list, which takes up 3/12 of the window. You might
-          choose to use <a href='https://mui.com/components/lists/'>Lists</a>{' '}
-          and <a href='https://mui.com/components/dividers/'>Dividers</a> to
-          display your users like so:
-        </Typography>
-        <List component='nav'>
-          {users.map((user) => (
-            <div key={user._id}>
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar
-                    alt={`${user.first_name}`}
-                    src='#'
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={<Link to={`/user/${user._id}`}>{user.first_name} {user.last_name}</Link>}
-                  key={user._id}
-                />
-              </ListItem>
-              <Divider />
-            </div>
-          ))}
-        </List>
-        <Typography variant='body1'>
-          The model comes in from window.models.userListModel()
-        </Typography>
-      </div>
+		(this.state.users.length === 0) ?
+		<p>Loading Users</p>
+		:
+		(
+		<div>
+			{/* <Typography variant='body1'>
+			This is the user list, which takes up 3/12 of the window. You might
+			choose to use <a href='https://mui.com/components/lists/'>Lists</a>{' '}
+			and <a href='https://mui.com/components/dividers/'>Dividers</a> to
+			display your users like so:
+			</Typography> */}
+			<List component='nav'>
+			{this.state.users.map((user) => (
+				<div key={user._id}>
+				<ListItem>
+					<ListItemAvatar>
+					<Avatar
+						alt={`${user.first_name}`}
+						src='#'
+					/>
+					</ListItemAvatar>
+					<ListItemText
+					primary={<Link to={`/user/${user._id}`}>{user.first_name} {user.last_name}</Link>}
+					key={user._id}
+					/>
+				</ListItem>
+				<Divider />
+				</div>
+			))}
+			</List>
+			<Typography variant='body1'>
+			The model comes in from window.models.userListModel()
+			</Typography>
+		</div>
+		)
     );
   }
 }
