4a55d9b707cb7b1bfef0456d13dbde4c82516bd4
Abhiram Jarugula <ajarugul@charlotte.edu>
2024-10-04 02:10:37 -0400

    userPhotos

--- components/userPhotos/userPhotos.jsx
+++ components/userPhotos/userPhotos.jsx
@@ -1,34 +1,139 @@
-import React from 'react';
-import {
-  Typography
-} from '@mui/material';
-import './userPhotos.css';
+import React, { useState, useEffect } from 'react';
+import { Typography, Button, Checkbox } from '@mui/material';
+import { Link, useParams, useHistory } from 'react-router-dom';
+import FetchModel from '../../lib/fetchModelData';
+import './userPhotos.css'; // Change this if you create a specific CSS for user photos
 
+function UserPhotos() {
+    const { userId, photoIndex } = useParams();
+    const [photos, setPhotos] = useState([]);
+    const [advancedFeaturesEnabled, setAdvancedFeaturesEnabled] = useState(false);
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+    const [showAdvancedFeatures, setShowAdvancedFeatures] = useState(false);
+    const history = useHistory();
 
-/**
- * Define UserPhotos, a React componment of project #5
- */
-class UserPhotos extends React.Component {
-  constructor(props) {
-    super(props);
+    useEffect(() => {
+        FetchModel(`/photosOfUser/${userId}`)
+            .then((response) => {
+                const userPhotos = response.data;
+                setPhotos(userPhotos);
 
-  }
+                if (photoIndex) {
+                    setAdvancedFeaturesEnabled(true);
+                    setCurrentPhotoIndex(parseInt(photoIndex));
+                }
+            })
+            .catch((error) => {
+                console.error('Error fetching user photos:', error);
+            });
+    }, [userId, photoIndex]);
 
-  render() {
+    const navigateToPhoto = (index) => {
+        if (index >= 0 && index < photos.length) {
+            setCurrentPhotoIndex(index);
+        }
+    };
+
+    const handleCheckboxChange = () => {
+        setShowAdvancedFeatures(!showAdvancedFeatures);
+    };
+
+    const handleGoBack = () => {
+        history.push(`/users/${userId}`);
+    };
+
+    const handleNextPhoto = () => {
+        if (currentPhotoIndex < photos.length - 1) {
+            setCurrentPhotoIndex(currentPhotoIndex + 1);
+        }
+    };
+
+    const handlePreviousPhoto = () => {
+        if (currentPhotoIndex > 0) {
+            setCurrentPhotoIndex(currentPhotoIndex - 1);
+        }
+    };
+
     return (
-      <Typography variant="body1">
-      This should be the UserPhotos view of the PhotoShare app. Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.models.photoOfUserModel(this.props.match.params.userId))}
-        </Typography>
-      </Typography>
+        <div className="user-photos-container">
+            <Button variant="contained" color="primary" onClick={handleGoBack}>
+                Go back to user details
+            </Button>
+            <Typography variant="h2">Photos</Typography>
+            <Checkbox
+                checked={showAdvancedFeatures}
+                onChange={handleCheckboxChange}
+                inputProps={{ 'aria-label': 'Show advanced features' }}
+            />
+            Show Advanced Features
 
+            {showAdvancedFeatures && (
+                <div className="photo-navigation">
+                    <Button variant="outlined" color="primary" onClick={handlePreviousPhoto}>
+                        Previous
+                    </Button>
+                    <Button variant="outlined" color="primary" onClick={handleNextPhoto}>
+                        Next
+                    </Button>
+                </div>
+            )}
+
+            {showAdvancedFeatures ? (
+                <div className="photo">
+                    <img src={`/images/${photos[currentPhotoIndex].file_name}`} alt={photos[currentPhotoIndex].file_name} />
+                    <p>Creation Date/Time: {photos[currentPhotoIndex].date_time}</p>
+                    <Typography variant="h3">Comments</Typography>
+                    {photos[currentPhotoIndex].comments && photos[currentPhotoIndex].comments.length > 0 ? (
+                        <ul className="comments">
+                            {photos[currentPhotoIndex].comments.map((comment) => (
+                                <li key={comment._id} className="comment">
+                                    <p>Comment Date/Time: {comment.date_time}</p>
+                                    <p>
+                                        Comment by:{' '}
+                                        <Link to={`/users/${comment.user._id}`}>
+                                            {`${comment.user.first_name} ${comment.user.last_name}`}
+                                        </Link>
+                                    </p>
+                                    <p>Comment: {comment.comment}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <Typography variant="body2">No comments for this photo</Typography>
+                    )}
+                </div>
+            ) : (
+                photos.map((photo, index) => (
+                    <div
+                        key={photo._id}
+                        className={`photo ${index === currentPhotoIndex ? 'visible' : 'hidden'}`}
+                    >
+                        <img src={`/images/${photo.file_name}`} alt={photo.file_name} />
+                        <p>Creation Date/Time: {photo.date_time}</p>
+                        <Typography variant="h3">Comments</Typography>
+                        {photo.comments && photo.comments.length > 0 ? (
+                            <ul className="comments">
+                                {photo.comments.map((comment) => (
+                                    <li key={comment._id} className="comment">
+                                        <p>Comment Date/Time: {comment.date_time}</p>
+                                        <p>
+                                            Comment by:{' '}
+                                            <Link to={`/users/${comment.user._id}`}>
+                                                {`${comment.user.first_name} ${comment.user.last_name}`}
+                                            </Link>
+                                        </p>
+                                        <p>Comment: {comment.comment}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <Typography variant="body2">No comments for this photo</Typography>
+                        )}
+                    </div>
+                ))
+            )}
+        </div>
     );
-  }
 }
 
 export default UserPhotos;
